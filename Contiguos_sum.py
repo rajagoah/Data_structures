a00395f6877d22be4ad0ccf25e54b6f734abfac8
rajagoah <rajagoah@mail.uc.edu>
2021-04-13 18:22:47 -0400

    Solution to find the sub-arry leading to the largest contiguous sum

--- Contiguos_sum.py
+++ Contiguos_sum.py
@@ -1,16 +1,25 @@
-a = [-2,1,-3,4,-1,2,1,-5,4]
-#a = [4,-1,2]
+class Contiguous_sum():
 
-index_position = 0
-sum_now= 0
-arr = [None]
-result_array = [None]
+    def __init__(self, array):
+        self.a = array
+        self.ending_slice = 0 #in order to extract the subset that results in highest contiguous sum
+        self.result_array = []  # to store the subset resulting in the highest contiguous sum
+        self.sum_now = 0 #to store the contiguous sum
+        self.arr = [None] #to store the values in the contiguous sum
 
-for k in range(len(a)):
-    sum_now = 0
-    for l in a[k:]:
-        sum_now = l + sum_now
-        if (None in arr) or (sum_now > arr[0]):
-            arr[0] = sum_now
-print(arr[0])
+    def contiguous_sum_array_finder(self):
+        for k in range(len(self.a)):
+            self.sum_now = 0
+            for l in self.a[k:]:
+                self.sum_now = l + self.sum_now
+                if (None in self.arr) or (self.sum_now > self.arr[0]):
+                    self.ending_slice = self.a[k:].index(l)
+                    self.result_array = self.a[k:]
+                    self.result_array = self.result_array[:self.ending_slice + 1]
+                    self.arr[0] = self.sum_now
+        return self.arr[0], self.result_array
 
+if __name__ == "__main__":
+    a = [-2,1,-3,4,-1,2,1,-5,4]
+    d = Contiguous_sum(a).contiguous_sum_array_finder()
+    print(d)
