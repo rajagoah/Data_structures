fd9736858ca3c54797be9ffd317abb9245506038
rajagoah <rajagoah@mail.uc.edu>
2021-04-12 22:40:48 -0400

    Corrected the iterator. Need to work on getting the sub-set that produces the highest contiguous sum

--- Contiguos_sum.py
+++ Contiguos_sum.py
@@ -1,13 +1,16 @@
 a = [-2,1,-3,4,-1,2,1,-5,4]
-#a = [1,2,3,4,5]
+#a = [4,-1,2]
 
+index_position = 0
 sum_now= 0
 arr = [None]
+result_array = [None]
 
 for k in range(len(a)):
-    if k != (len(a) - 1):
-        sum_now = a[k] + sum_now
-        if None in arr or sum_now > arr[0]:
+    sum_now = 0
+    for l in a[k:]:
+        sum_now = l + sum_now
+        if (None in arr) or (sum_now > arr[0]):
             arr[0] = sum_now
-    k += 1
-print(arr)
+print(arr[0])
+
