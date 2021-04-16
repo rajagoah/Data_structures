449ae434ca88fe864f9a11338d8a8cea444bf9f0
Aakarsh Yoga 7i <rajagoh@mail.uc.edu>
2021-04-15 21:42:58 -0400

    FInished up the code

--- String_compression.py
+++ String_compression.py
@@ -1,21 +1,26 @@
-a = 'AAaaBBCcccccx01982'
+class String_Compression():
 
-counter = 1
+    def __init__(self, array):
+        self.array = array #to store input strings
+        self.counter = 1 #to count the number of occurrence of the letter
+        self.dic = dict() #to store the mapping of occurrences to the letters
+        self.strng = list() #to sotre the value such as--['A1', 'B2']
 
-dic = dict()
-strng = list()
+    def compressor(self):
+        for i in range(len(self.array)):
+            if self.array[i] not in self.dic: #validating if the element exists in the dictionary
+                self.dic[self.array[i]] = self.counter #if not there then add the value of the counter
+            else:
+                self.dic[self.array[i]] = self.dic[self.array[i]] + 1 #if it exists then increment the value
 
-for k in range(len(a)):
-    if a[k] not in dic:
-        dic[a[k]] = counter
-    else:
-        dic[a[k]] = dic[a[k]] + 1
+        for k, v in self.dic.items(): #once parsed through the value in the input string, then parse through the dic items and combine them
+            self.strng.append(k + str(v)) #First combine the key and value and then append the values in the dic to the list
+        return ''.join(self.strng) #use the join operation to convert the list to strings
 
-print(dic)
-str
-for k,v in dic.items():
-    strng.append(k+ str(v))
-print(''.join(strng))
+if __name__ == "__main__":
+    a = 'AsSAAdytza'
+    d = String_Compression(a).compressor()
+    print(d)
 
 
 
